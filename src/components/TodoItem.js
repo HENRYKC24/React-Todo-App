de4e30721f85a541a930093fabc9e5cba4f24d8f
HENRY-KC <henrykc24@yahoo.com>
2021-09-20 21:47:25 +0100

    Create Todo Item

---
+++ src/components/TodoItem.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styles from './TodoItem.module.css';
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+    };
+  }
+
+  componentWillUnmount() {
+    console.log('Cleaning up...');
+  }
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false });
+    }
+  }
+
+  render() {
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#595959',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+
+    const {
+      handleChangeProps,
+      todo: { completed, id, title },
+      setUpdate,
+      deleteTodoProps,
+    } = this.props;
+
+    const viewMode = {};
+    const editMode = {};
+
+    const { editing } = this.state;
+
+    if (editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
+    }
+
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => handleChangeProps(id)}
+          />
+          <button type="button" onClick={() => deleteTodoProps(id)}>Delete</button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={(e) => {
+            setUpdate(e.target.value, id);
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+      </li>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  handleChangeProps: propTypes.func.isRequired,
+  todo: propTypes.instanceOf(Object).isRequired,
+  setUpdate: propTypes.func.isRequired,
+  deleteTodoProps: propTypes.func.isRequired,
+};
+
+export default TodoItem;
