e8bf9bf43a8f51dc375dee9c64a4a9d94a593cba
HENRY-KC <henrykc24@yahoo.com>
2021-09-21 15:03:47 +0100

    Wrap up new index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoContainer from './components/TodoContainer';
-import './App.css';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import TodoContainer from './functionBased/components/TodoContainer';
+
+import './functionBased/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router basename={process.env.PUBLIC_URL}>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
